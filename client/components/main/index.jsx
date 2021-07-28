a304e73f90955df286b5c95c31c59220b41fe061
Nick Ng <nicholas.cj.ng@gmail.com>
2021-07-29 11:30:47 +1200

    added sound

--- client/components/main/index.jsx
+++ client/components/main/index.jsx
@@ -3,9 +3,15 @@ import styled from "styled-components";
 import io from "socket.io-client";
 
 import Listing from "./listing";
+import VoiceChooser from "./voice-chooser";
+import { getVoices, sayWithVoice } from "./text-to-speech";
 import { trimListings, filterListingsChaos } from "./utils";
 
 const MAX_CHAOS = "MAX_CHAOS";
+const VOICE_CHARACTER = "VOICE_CHARACTER";
+const VOICE_VOLUME = "VOICE_VOLUME";
+const VOICE_VERBOSE = "VOICE_VERBOSE";
+const VOICE_COOLDOWN_MS = 1000;
 
 const Container = styled.div``;
 
@@ -15,8 +21,22 @@ const Columns = styled.div`
   gap: 0.5em;
 `;
 
-const Controls = styled.div`
+const HorizontalControls = styled.div`
   margin-bottom: 0.5em;
+`;
+
+const VerticalControls = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > * {
+    margin-bottom: 0.5em;
+  }
+
+  label {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 export default function Main() {
@@ -26,8 +46,25 @@ export default function Main() {
     parseInt(localStorage.getItem(MAX_CHAOS) || 100, 10)
   );
   const [message, setMessage] = useState("Please wait.");
+  const [voices, setVoices] = useState([]);
+  const [voice, setVoice] = useState(
+    localStorage.getItem(VOICE_CHARACTER) || ""
+  );
+  const [voiceVolume, setVoiceVolume] = useState(
+    parseFloat(localStorage.getItem(VOICE_VOLUME)) || 0.3
+  );
+  const [verbose, setVerbose] = useState(
+    localStorage.getItem(VOICE_VERBOSE) === "true"
+  );
+  const [lastSay, setLastSay] = useState(0);
 
   useEffect(() => {
+    const bb = async () => {
+      const voices = await getVoices();
+      setVoices(voices);
+    };
+    bb();
+
     const socket = io();
 
     socket.on("connect", () => {
@@ -39,6 +76,7 @@ export default function Main() {
         ...data,
         timeStamp: Date.now(),
       };
+
       setNewListings((prevNewListings) =>
         trimListings([listing].concat(prevNewListings))
       );
@@ -46,10 +84,35 @@ export default function Main() {
     });
   }, []);
 
+  useEffect(() => {
+    if (newListings.length === 0) {
+      return;
+    }
+    if (Date.now() - lastSay < VOICE_COOLDOWN_MS) {
+      return;
+    }
+
+    const newest = filterListingsChaos([newListings[0]], maxChaos).pop();
+
+    if (newest) {
+      const phrase = `${newest.note}${
+        newest.price
+          ? ` for ${newest.price.amount} ${newest.price.currency}`
+          : ""
+      }`;
+      if (verbose && phrase) {
+        sayWithVoice(phrase, voice, voiceVolume);
+      } else {
+        sayWithVoice("woop!", voice, voiceVolume);
+      }
+      setLastSay(Date.now());
+    }
+  }, [newListings]);
+
   return (
     <Container>
       <h1>Trade Search Listings</h1>
-      <Controls>
+      <HorizontalControls>
         <label>
           Max Chaos:{" "}
           <input
@@ -62,7 +125,7 @@ export default function Main() {
             value={maxChaos}
           />
         </label>
-      </Controls>
+      </HorizontalControls>
       <Columns>
         <div>
           {filterListingsChaos(newListings, maxChaos).map((listing) => (
@@ -85,7 +148,40 @@ export default function Main() {
             <Listing listing={listing} key={listing.id} onClick={() => {}} />
           ))}
         </div>
-        <div>WIP</div>
+        <VerticalControls>
+          <VoiceChooser
+            voices={voices}
+            voice={voice}
+            onChange={(newVoice) => {
+              setVoice(newVoice);
+              localStorage.setItem(VOICE_CHARACTER, newVoice);
+            }}
+          />
+          <label>
+            Volume:{" "}
+            <input
+              value={voiceVolume}
+              type="number"
+              min="0"
+              max="1"
+              step="0.05"
+              onChange={(e) => {
+                setVoiceVolume(parseFloat(e.target.value));
+                localStorage.setItem(VOICE_VOLUME, e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Descriptive:{" "}
+            <input
+              checked={verbose}
+              type="checkbox"
+              onChange={(e) => {
+                setVerbose(e.target.checked);
+              }}
+            />
+          </label>
+        </VerticalControls>
       </Columns>
       <pre>{message}</pre>
     </Container>
---
+++ client/components/main/text-to-speech.js
@@ -0,0 +1,28 @@
+const synth = window.speechSynthesis;
+
+const sleep = (ms, output = null) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(output);
+    });
+  });
+
+export const getVoices = async () => {
+  for (let n = 0; n < 10000; n++) {
+    if (typeof synth.getVoices === "function") {
+      return synth.getVoices();
+    }
+    await sleep(50);
+  }
+};
+
+export const sayWithVoice = (phrase, voiceURI, volume = 0.5, rate = 1.5) => {
+  const voice = synth.getVoices().find((voice) => voice.voiceURI === voiceURI);
+  const utterance = new SpeechSynthesisUtterance(phrase);
+  utterance.volume = volume;
+  utterance.rate = rate;
+  if (voice) {
+    utterance.voice = voice;
+    synth.speak(utterance);
+  }
+};
